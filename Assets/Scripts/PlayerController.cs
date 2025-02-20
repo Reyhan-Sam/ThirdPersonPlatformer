f50f487c69cb3f17f9a880d19fa2a0ddfbadc751
Reyhan-Sam <reyhansam13@gmail.com>
2025-02-20 14:49:42 -0800

    added cinemachine for free look camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float force = 5f;
+    [SerializeField] private Transform cameraTransform;
     [SerializeField] private InputManager inputManager;
     private Rigidbody playerRB;
     private Vector3 moveDirection = Vector3.zero;
@@ -16,7 +17,13 @@ public class PlayerController : MonoBehaviour
 
     private void Move(Vector3 direction)
     {
-        moveDirection = new Vector3(direction.x, 0, direction.z).normalized;
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        // cameraForward.Normalize();
+        // cameraRight.Normalize();
+        moveDirection = cameraForward * direction.z + cameraRight * direction.x;
 
     }
 
